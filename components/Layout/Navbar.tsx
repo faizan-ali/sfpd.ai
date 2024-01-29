a486d9b092b3d2219ee7db088732071f3fdcf92e
Faizan Ali <faizanali619@gmail.com>
2024-01-28 21:57:12 -0800

    improve navbar

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,9 +1,12 @@
 import {FC} from "react";
+import Image from 'next/image'
+
 
 export const Navbar: FC = () => {
     return (
-        <nav className="bg-navy-blue tssext-white px-4 py-2 flex justify-between items-center">
-            <div className="text-lg font-bold text-gold">SFPD.ai</div>
+        <nav className="bg-navy-blue tssext-white px-4 py-2 flex items-center">
+            <Image src='https://www.sanfranciscopolice.org/themes/custom/sfpd/logo.svg' alt='SFPD Logo' width={50} height={50}/>
+            <div className="text-lg font-bold text-gold ml-2">SFPD.ai</div>
         </nav>
     );
 };
