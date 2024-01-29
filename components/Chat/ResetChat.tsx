ebf47234c6b8c81be8e33c140ff1aac1460e0f9c
Faizan Ali <faizanali619@gmail.com>
2024-01-28 21:55:06 -0800

    remove border, improve reset

--- components/Chat/ResetChat.tsx
+++ components/Chat/ResetChat.tsx
@@ -8,7 +8,7 @@ export const ResetChat: FC<Props> = ({ onReset }) => {
   return (
     <div className="flex flex-row items-center">
       <button
-        className="text-sm sm:text-base text-neutral-900 font-semibold rounded-lg px-4 py-2 bg-neutral-200 hover:bg-neutral-300 focus:outline-none focus:ring-1 focus:ring-neutral-300"
+        className="text-xs underline text-neutral-900 font-semibold rounded-lg px-4 py-2 hover:bg-neutral-300 focus:outline-none focus:ring-1 focus:ring-neutral-300"
         onClick={() => onReset()}
       >
         Reset
